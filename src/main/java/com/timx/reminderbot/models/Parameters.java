9269ddd0dfa7194b0887723e0fe1fb8077e5d60b
Agustinus Agri Ardyan <Agustinus_A7234@mitrais.com>
2017-10-03 17:42:44 +0800

    add @Data for pojo

--- src/main/java/com/timx/reminderbot/models/Parameters.java
+++ src/main/java/com/timx/reminderbot/models/Parameters.java
@@ -4,7 +4,9 @@ package com.timx.reminderbot.models;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
 
+@Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
     "date",
--- src/main/java/com/timx/reminderbot/models/Result.java
+++ src/main/java/com/timx/reminderbot/models/Result.java
@@ -5,7 +5,9 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
 
+@Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
     "source",
