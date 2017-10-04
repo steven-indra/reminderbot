328232c0bdf4236c5e116a9de3e1e43a6bbd8bb3
Steven Indra Adinata <steven.indra93@gmail.com>
2017-10-04 14:08:44 +0800

    test2

--- src/main/java/com/timx/reminderbot/SampleController.java
+++ src/main/java/com/timx/reminderbot/SampleController.java
@@ -38,6 +38,7 @@ public class SampleController {
 
     @RequestMapping(value = "/view", method = RequestMethod.GET)
     public String view() {
+    	String URL = "https://api.api.ai/v1/query";
         String date = DateUtils.getTodayDate();
         String sessionId = UUID.randomUUID().toString();
         RestTemplate rt = new RestTemplate();
@@ -45,13 +46,32 @@ public class SampleController {
         headers.set("Authorization", "Bearer 1ab5cef3becc432eb54a79bacf5f7d85");
         HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
         
-        // iki ven
-        UriComponents uriComponents = UriComponentsBuilder.newInstance()
-                .scheme("https").host("api.api.ai")
-                .path("/v1").path("/query").query("v={keyword}").buildAndExpand("show rock concerts in december");
+        text = "remind to go to gym this afternoon";
         
-        ResponseEntity<ApiAiJSON> exchange = rt.exchange(uriComponents.toUri(), HttpMethod.GET, entity, ApiAiJSON.class);
+        UriComponentsBuilder uriComponents = UriComponentsBuilder.fromUriString(URL)
+					                			.queryParam("v", date)
+					                			.queryParam("query", text)
+					                			.queryParam("sessionId", sessionId);
+        
+        log.info("Uri builder: ", uriComponents.build().toUri());
+        log.info("Uri builder: ", uriComponents.build().toString());
+        
+        ResponseEntity<ApiAiJSON> exchange = rt.exchange(uriComponents.build().toUri(), HttpMethod.GET, entity, ApiAiJSON.class);
         return "OK : " + exchange.getBody().getResult().getMetadata().getIntentName();
+//        String date = DateUtils.getTodayDate();
+//        String sessionId = UUID.randomUUID().toString();
+//        RestTemplate rt = new RestTemplate();
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.set("Authorization", "Bearer 1ab5cef3becc432eb54a79bacf5f7d85");
+//        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+//        
+//        // iki ven
+//        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+//                .scheme("https").host("api.api.ai")
+//                .path("/v1").path("/query").query("v={keyword}").buildAndExpand("show rock concerts in december");
+//        
+//        ResponseEntity<ApiAiJSON> exchange = rt.exchange(uriComponents.toUri(), HttpMethod.GET, entity, ApiAiJSON.class);
+//        return "OK : " + exchange.getBody().getResult().getMetadata().getIntentName();
     }
 
 }
